b158a0a492dcd309217872178fc8c6d267e37f77
Alejandro Toledo <90484079+Deathlezz300@users.noreply.github.com>
2023-12-07 08:53:45 -0500

    update table

--- FRONT-END/src/Components/Tabla.tsx
+++ FRONT-END/src/Components/Tabla.tsx
@@ -7,24 +7,24 @@ interface props{
 
 export const Tabla:FC<props> = ({columnas,filas}) => {
   return (
-    <table style={{borderCollapse:'separate',borderSpacing:'7px'}} className='divide-white  w-[100%] border-2 border-white'>
-      <thead className='divide-white'>
-        <tr >
+    <table style={{borderCollapse:'separate',borderSpacing:'0px'}} className='bg-[#121212] divide-white rounded-lg  w-[100%] shadow-md'>
+      <thead className='bg-[#181818] rounded-lg'>
+        <tr className='bg-[#181818] rounded-lg'>
           {
             columnas.map((colum)=>{
-              return <th className='text-white font-rubik text-left border-2 border-white'>{colum}</th>
+              return <th className='text-white font-rubik text-center p-2 py-4 rounded-lg'>{colum}</th>
             })
           }
         </tr>
       </thead>
-      <tbody className='divide-white'>
+      <tbody>
           {
             filas.map(value=>{
               return(
-                <tr>
+                <tr >
                   {
                     value.map((dato:any)=>{
-                      return <td className='text-white font-rubik '>{dato}</td>
+                      return <td className='text-white font-rubik p-4 border border-[#1E1E1E]'>{dato}</td>
                     })
                   }
                 </tr>
